a537b048e12a526f87149d2caaabdc46f75b615b
Oswaldo Aguilar Martinez <69224700+Waldo97Ag@users.noreply.github.com>
2021-01-28 14:49:35 -0600

    Add files via upload

---
+++ Archivo_Registros/Archivo_Registros.srcs/sim_1/new/tb_archivoR.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 25.11.2020 20:15:50
+// Design Name: 
+// Module Name: tb_archivoR
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_archivoR(
+
+    );
+endmodule
